67df89cc01847e5144b268f817fa1cc5847d99a4
DEGEN101 <ntando.raji13@gmail.com>
2025-05-18 20:05:21 +0200

    feat(models): updated /models/index.js to accommodate newly addded models

--- models/index.js
+++ models/index.js
@@ -25,11 +25,41 @@ const database = {};
 database.sql = sql;
 database.poolPromise = poolPromise;
 
+// --- Employee Model ---
 database.employees = require("./employees.model.js")(sql, poolPromise);
-database.stockItem = require("./stockItem.model.js")(sql, poolPromise);
+// ----------------------
+
+// --- Inventory Related Models ---
 database.inventory = require("./inventory.model.js")(sql, poolPromise);
+database.location = require("./location.model.js")(sql, poolPromise);
+// --------------------------------
+
+// --- Purchases Related Models ---
+database.supplier = require("./supplier.model.js")(sql, poolPromise);
+database.purchaseOrder = require("./purchaseOrder.model.js")(sql, poolPromise);
+database.purchaseOrder = require("./purchaseOrderDetails.model.js")(sql, poolPromise);
+// -------------------------------------------
+
+// --- Stock Item Related Models ---
+database.stockItem = require("./stockItem.model.js")(sql, poolPromise);
+database.stockItemAttribute = require("./stockItemAttribute.model.js")(sql, poolPromise);
+database.stockItemAttributeValue = require("./stockItemAttributeValue.model.js")(sql, poolPromise);
 database.stockItemCategory = require("./stockItemCategory.model.js")(sql, poolPromise);
 database.stockItemGroup = require("./stockItemGroup.model.js")(sql, poolPromise);
+// --------------------------------
+
+// --- Unit of Measurement Related Models ---
 database.uom = require("./uom.model.js")(sql ,poolPromise);
+database.uomConversion = require("./uomConversion.model.js")(sql ,poolPromise);
+// ------------------------------------------
+
+// --- Production Related Models ---
+database.product = require("./product.model.js")(sql ,poolPromise);
+// ---------------------------------
+
+
+// --- Logging Related Models ---
+database.auditLog = require("./auditLog.model.js")(sql ,poolPromise);
+// -----------------------------
 
 module.exports = database;
