a5edac9b0e7917a73037b889cb26399130ca5aad
DEGEN101 <ntando.raji13@gmail.com>
2025-05-18 22:18:19 +0200

    wrote initial swagger comments for employee api routes

--- routes/employee.routes.js
+++ routes/employee.routes.js
@@ -1,3 +1,119 @@
+/**
+ * @swagger
+ * tags:
+ *   - name: Employees
+ *     description: Employee management
+ */
+
+/**
+ * @swagger
+ * /api/employees:
+ *   get:
+ *     summary: Get all employees
+ *     tags: [Employees]
+ *     responses:
+ *       200:
+ *         description: List of employees
+ */
+
+/**
+ * @swagger
+ * /api/employees/{id}:
+ *   get:
+ *     summary: Get an employee by ID
+ *     tags: [Employees]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Employee found
+ *       404:
+ *         description: Employee not found
+ */
+
+/**
+ * @swagger
+ * /api/employees:
+ *   post:
+ *     summary: Create a new employee
+ *     tags: [Employees]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               FirstName:
+ *                 type: string
+ *               Surname:
+ *                 type: string
+ *               Role:
+ *                 type: string
+ *               Email:
+ *                 type: string
+ *               PhoneNumber:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Created
+ */
+
+/**
+ * @swagger
+ * /api/employees/{id}:
+ *   put:
+ *     summary: Update an employee by ID
+ *     tags: [Employees]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               FirstName:
+ *                 type: string
+ *               Surname:
+ *                 type: string
+ *               Role:
+ *                 type: string
+ *               Email:
+ *                 type: string
+ *               PhoneNumber:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Updated
+ */
+
+/**
+ * @swagger
+ * /api/employees/{id}:
+ *   delete:
+ *     summary: Delete an employee
+ *     tags: [Employees]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Deleted
+ */
+
 const registerCrudRoutes = require("../utils/registerCrudRoutes.js");
 
 module.exports = (app) => {
