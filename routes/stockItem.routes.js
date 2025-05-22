41c16808b4d87d44d6f29ee4a5f55b22799ced17
DEGEN101 <ntando.raji13@gmail.com>
2025-05-22 10:43:04 +0200

    added swagger api docs to supplier and stockItem routes

--- routes/stockItem.routes.js
+++ routes/stockItem.routes.js
@@ -1,3 +1,142 @@
+/**
+ * @swagger
+ * tags:
+ *   - name: StockItems
+ *     description: Stock item management
+ */
+
+/**
+ * @swagger
+ * /api/stockItem:
+ *   get:
+ *     summary: Get all stock items
+ *     tags: [StockItems]
+ *     responses:
+ *       200:
+ *         description: List of stock items
+ */
+
+/**
+ * @swagger
+ * /api/stockItem/{id}:
+ *   get:
+ *     summary: Get a stock item by ID
+ *     tags: [StockItems]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Stock item found
+ *       404:
+ *         description: Stock item not found
+ */
+
+/**
+ * @swagger
+ * /api/stockItem/sku/{sku}:
+ *   get:
+ *     summary: Get a stock item by SKU
+ *     tags: [StockItems]
+ *     parameters:
+ *       - in: path
+ *         name: sku
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Stock item found
+ *       404:
+ *         description: Stock item not found
+ */
+
+/**
+ * @swagger
+ * /api/stockItem:
+ *   post:
+ *     summary: Create a new stock item
+ *     tags: [StockItems]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               Name:
+ *                 type: string
+ *               SKU:
+ *                 type: string
+ *               Description:
+ *                 type: string
+ *               StockItemCategoryID:
+ *                 type: integer
+ *               BaseUoMID:
+ *                 type: integer
+ *               GroupID:
+ *                 type: integer
+ *     responses:
+ *       201:
+ *         description: Created
+ */
+
+/**
+ * @swagger
+ * /api/stockItem/{id}:
+ *   put:
+ *     summary: Update a stock item by ID
+ *     tags: [StockItems]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               Name:
+ *                 type: string
+ *               SKU:
+ *                 type: string
+ *               Description:
+ *                 type: string
+ *               StockItemCategoryID:
+ *                 type: integer
+ *               BaseUoMID:
+ *                 type: integer
+ *               GroupID:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Updated
+ */
+
+/**
+ * @swagger
+ * /api/stockItem/{id}:
+ *   delete:
+ *     summary: Delete a stock item
+ *     tags: [StockItems]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Deleted
+ */
+
 const registerCrudRoutes = require("../utils/registerCrudRoutes.js");
 
 module.exports = (app) => {
--- routes/supplier.routes.js
+++ routes/supplier.routes.js
@@ -1,3 +1,112 @@
+/**
+ * @swagger
+ * tags:
+ *   - name: Suppliers
+ *     description: Supplier management
+ */
+
+/**
+ * @swagger
+ * /api/supplier:
+ *   get:
+ *     summary: Get all suppliers
+ *     tags: [Suppliers]
+ *     responses:
+ *       200:
+ *         description: List of suppliers
+ */
+
+/**
+ * @swagger
+ * /api/supplier/{id}:
+ *   get:
+ *     summary: Get a supplier by ID
+ *     tags: [Suppliers]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Supplier found
+ *       404:
+ *         description: Supplier not found
+ */
+
+/**
+ * @swagger
+ * /api/supplier:
+ *   post:
+ *     summary: Create a new supplier
+ *     tags: [Suppliers]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               Name:
+ *                 type: string
+ *               Email:
+ *                 type: string
+ *               PhoneNumber:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Created
+ */
+
+/**
+ * @swagger
+ * /api/supplier/{id}:
+ *   put:
+ *     summary: Update a supplier by ID
+ *     tags: [Suppliers]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               Name:
+ *                 type: string
+ *               Email:
+ *                 type: string
+ *               PhoneNumber:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Updated
+ */
+
+/**
+ * @swagger
+ * /api/supplier/{id}:
+ *   delete:
+ *     summary: Delete a supplier
+ *     tags: [Suppliers]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Deleted
+ */
+
+
 const registerCrudRoutes = require("../utils/registerCrudRoutes");
 
 module.exports = (app) => {
