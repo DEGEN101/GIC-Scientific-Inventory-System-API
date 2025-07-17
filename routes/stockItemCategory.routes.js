d444c53b2a6f5f1f25e87a74571810d2d02a1f44
DEGEN101 <ntando.raji13@gmail.com>
2025-07-17 12:33:02 +0200

    Added swagger docs for routes: StockItemCategory and StockItemGroup

--- routes/stockItemCategory.routes.js
+++ routes/stockItemCategory.routes.js
@@ -1,3 +1,107 @@
+/**
+ * @swagger
+ * tags:
+ *   - name: StockItemCategory
+ *     description: Manage stock item categories
+ */
+
+/**
+ * @swagger
+ * /api/stockItemCategory:
+ *   get:
+ *     summary: Get all stock item categories
+ *     tags: [StockItemCategory]
+ *     responses:
+ *       200:
+ *         description: List of stock item categories
+ */
+
+/**
+ * @swagger
+ * /api/stockItemCategory/{id}:
+ *   get:
+ *     summary: Get a stock item category by ID
+ *     tags: [StockItemCategory]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Stock item category found
+ *       404:
+ *         description: Stock item category not found
+ */
+
+/**
+ * @swagger
+ * /api/stockItemCategory:
+ *   post:
+ *     summary: Create a new stock item category
+ *     tags: [StockItemCategory]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               CategoryName:
+ *                 type: string
+ *               Description:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Created
+ */
+
+/**
+ * @swagger
+ * /api/stockItemCategory/{id}:
+ *   put:
+ *     summary: Update a stock item category by ID
+ *     tags: [StockItemCategory]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               CategoryName:
+ *                 type: string
+ *               Description:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Updated
+ */
+
+/**
+ * @swagger
+ * /api/stockItemCategory/{id}:
+ *   delete:
+ *     summary: Delete a stock item category
+ *     tags: [StockItemCategory]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Deleted
+ */
+
 const registerCrudRoutes = require("../utils/registerCrudRoutes.js");
 
 module.exports = (app) => {
--- routes/stockItemGroup.routes.js
+++ routes/stockItemGroup.routes.js
@@ -1,3 +1,107 @@
+/**
+ * @swagger
+ * tags:
+ *   - name: StockItemGroup
+ *     description: Manage stock item groups
+ */
+
+/**
+ * @swagger
+ * /api/StockItemGroup:
+ *   get:
+ *     summary: Get all stock item groups
+ *     tags: [StockItemGroup]
+ *     responses:
+ *       200:
+ *         description: List of stock item groups
+ */
+
+/**
+ * @swagger
+ * /api/StockItemGroup/{id}:
+ *   get:
+ *     summary: Get a stock item group by ID
+ *     tags: [StockItemGroup]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Stock item group found
+ *       404:
+ *         description: Stock item group not found
+ */
+
+/**
+ * @swagger
+ * /api/StockItemGroup:
+ *   post:
+ *     summary: Create a new stock item group
+ *     tags: [StockItemGroup]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               GroupName:
+ *                 type: string
+ *               Description:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Created
+ */
+
+/**
+ * @swagger
+ * /api/StockItemGroup/{id}:
+ *   put:
+ *     summary: Update a stock item group by ID
+ *     tags: [StockItemGroup]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               GroupName:
+ *                 type: string
+ *               Description:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Updated
+ */
+
+/**
+ * @swagger
+ * /api/StockItemGroup/{id}:
+ *   delete:
+ *     summary: Delete a stock item group
+ *     tags: [StockItemGroup]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Deleted
+ */
+
 const registerCrudRoutes = require("../utils/registerCrudRoutes");
 
 module.exports = (app) => {
